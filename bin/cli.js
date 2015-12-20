0701af1d4033b377e640231a4303685ec0bd7a7f
Milk Lee <me@limingkai.cn>
2015-12-20 19:04:38 +0800

    基本完成

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,8 @@ const fs = require( 'fs' ) ,
   path = require( 'path' ) ,
   XLSX = require( 'xlsx' );
 
+var cons = require( 'consolidate' );
+
 const templates = fs.readdirSync( path.resolve( __dirname , '../templates' ) );
 
 const files = fs.readdirSync( './' );
@@ -18,5 +20,14 @@ function handle( dirPath ) {
   // 读取电子表格的第一张表
   const wb = XLSX.readFile( path.resolve( dirPath , 'data.xlsx' ) );
   const firstSheet = wb.Sheets[ wb.SheetNames[ 0 ] ];
-  console.log( XLSX.utils.sheet_to_json( firstSheet ) );
+  const array = XLSX.utils.sheet_to_json( firstSheet );
+
+  // 读取模板路径
+  const template = path.resolve( __dirname , '../templates/' + dirPath + '/index.html' );
+
+  array.forEach( function ( data ) {
+    cons.dot( template , data ).then( function ( html ) {
+      fs.writeFileSync( dirPath + '/' + data[ '分单号' ] + '.html' , html );
+    } );
+  } );
 }
