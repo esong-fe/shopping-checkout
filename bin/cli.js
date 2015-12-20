0f9f04f256a4aba84cefeba7f9ce74bb1d527362
Milk Lee <me@limingkai.cn>
2015-12-20 22:43:22 +0800

    功能完成,明天导入模板后试一试

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,20 @@
 #! /usr/bin/env node
 
-const fs = require( 'fs' ) ,
+const xlsxFilename = 'data.xlsx';
+
+const fs = require( 'fs-extra' ) ,
   path = require( 'path' ) ,
-  XLSX = require( 'xlsx' );
+  XLSX = require( 'xlsx' ) ,
+  cons = require( 'consolidate' );
 
-var cons = require( 'consolidate' );
+const resolvePath = path.resolve;
 
-const templates = fs.readdirSync( path.resolve( __dirname , '../templates' ) );
+const workDir = process.cwd() ,
+  packageDir = path.resolve( __dirname , '../' );
 
-const files = fs.readdirSync( './' );
+const templates = fs.readdirSync( resolvePath( packageDir , './templates' ) );
+
+const files = fs.readdirSync( workDir );
 
 files.forEach( function ( filename ) {
   if ( templates.indexOf( filename ) >= 0 && fs.statSync( filename ).isDirectory() ) {
@@ -18,16 +24,23 @@ files.forEach( function ( filename ) {
 
 function handle( dirPath ) {
   // 读取电子表格的第一张表
-  const wb = XLSX.readFile( path.resolve( dirPath , 'data.xlsx' ) );
+  const wb = XLSX.readFile( resolvePath( dirPath , xlsxFilename ) );
   const firstSheet = wb.Sheets[ wb.SheetNames[ 0 ] ];
   const array = XLSX.utils.sheet_to_json( firstSheet );
+  if ( array.length ) {
+    // 读取模板路径
+    const template = resolvePath( packageDir , './templates/' + dirPath + '/index.html' );
 
-  // 读取模板路径
-  const template = path.resolve( __dirname , '../templates/' + dirPath + '/index.html' );
+    // 复制模板所需的静态文件
+    fs.copySync(
+      resolvePath( packageDir , './templates/' + dirPath + '/resources' ) ,
+      resolvePath( workDir , './' + dirPath + '/resources' )
+    );
 
-  array.forEach( function ( data ) {
-    cons.dot( template , data ).then( function ( html ) {
-      fs.writeFileSync( dirPath + '/' + data[ '分单号' ] + '.html' , html );
+    array.forEach( function ( data ) {
+      cons.dot( template , data ).then( function ( html ) {
+        fs.writeFileSync( resolvePath( workDir , dirPath + '/' + data[ '分单号' ] + '.html' ) , html );
+      } );
     } );
-  } );
+  }
 }
