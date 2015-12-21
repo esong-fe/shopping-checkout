ae48774bd75b84e8e2f61e61cb4aa0ebb21f3073
Milk Lee <milk.lee@qq.com>
2015-12-21 17:03:55 +0800

    添加运行日志

--- bin/cli.js
+++ bin/cli.js
@@ -9,9 +9,19 @@ const fs = require( 'fs-extra' ) ,
   path = require( 'path' ) ,
   parseXLSX = require( 'xlsx' ).readFile;
 
+const pkg = require( '../package.json' );
+
 const pathResolve = path.resolve ,
   workDir = process.cwd() , // 当前工作目录
   packageDir = path.resolve( __dirname , '../' ); // 此 npm 包在本地系统的路径，用于定位模板位置
+
+log( '小票工具 v' + pkg.version );
+log( '问题或建议请联系 ' + pkg.author.email );
+log( '\n开始运行小票工具 :)' );
+
+// 记录运行数据
+let templateCount = 0 , // 寻找到了多少个模板
+  htmlCount = 0; // 一共生成了多少个 html 文件
 
 Promise.all( [
   new Promise( ( resolve , reject )=> {
@@ -35,11 +45,19 @@ Promise.all( [
 ] ).then( ( args )=> {
   const templates = args[ 0 ] , files = args[ 1 ];
 
+  log( '\n目前支持的模板有：' );
+  templates.forEach( function ( tpn ) {
+    log( tpn );
+  } );
+
+  log( '\n正在 ' + workDir + ' 里寻找是否有匹配的模板……' );
+
   files.forEach( ( maybeTemplateName )=> {
     if ( templates.indexOf( maybeTemplateName ) >= 0 ) { // 如果文件夹的名字是其中一个模板的名字
       fs.stat( maybeTemplateName , ( err , stat )=> {
         if ( err ) {
-          console.log( '查询文件状态时出错：' , err );
+          log( '查询文件状态时出错：' );
+          log( err );
         } else if ( stat.isDirectory() ) {
           handle( maybeTemplateName );
         }
@@ -48,13 +66,19 @@ Promise.all( [
   } );
 
   function handle( templateName ) {
-    console.log( '找到模板：' , templateName );
+    log( '找到模板：' + templateName );
+    templateCount += 1;
 
     // 复制模板所需的静态文件
     fs.copy(
       pathResolve( packageDir , './libs/templates/' + templateName + '/resources' ) ,
       pathResolve( workDir , './' + templateName + '/resources' ) ,
-      ( err )=> err && console.log( '复制文件至目标文件夹时出错：' , err )
+      ( err )=> {
+        if ( err ) {
+          log( '复制文件至目标文件夹时出错：' );
+          log( err );
+        }
+      }
     );
 
     // 读取电子表格的第一张表
@@ -63,15 +87,26 @@ Promise.all( [
 
     const renderFunc = require( '../libs/templates/' + templateName + '/index.js' );
     renderFunc( firstSheet , function onRendered( rowData , html ) {
-      fs.writeFile( pathResolve( workDir , templateName + '/' + rowData[ '分单号' ] + '.html' ) , html , ( err )=> {
+      const destPath = pathResolve( workDir , templateName + '/' + rowData[ '分单号' ] + '.html' );
+
+      fs.writeFile( destPath , html , ( err )=> {
         if ( err ) {
-          console.log( '写入文件时出错：' , err );
+          log( '写入文件时出错：' + err );
         } else {
-          console.log( '模板' , templateName , '下生成文件：' , rowData[ '分单号' ] + '.html' );
+          htmlCount += 1;
+          log( '生成文件：' + destPath );
         }
       } );
     } );
   }
 } , ( err )=> {
-  console.log( '读取时发生错误：' , err );
+  log( '读取时发生错误：' + err );
 } );
+
+process.on( 'exit' , function () {
+  log( '\n运行完毕，此次运行发现了 ' + templateCount + ' 个模板文件夹，共生成 ' + htmlCount + ' 个 html 文件。' );
+} );
+
+function log( msg ) {
+  console.log( msg );
+}
