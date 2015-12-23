8c4991dbceb7590fd7ac7e28fe28f4397d3ceb4f
Milk Lee <milk.lee@qq.com>
2015-12-23 13:49:16 +0800

    使用 proccess.chdir 切换工作目录，这样可以减少很多 path.resolve 的调用

--- libs/index.js
+++ libs/index.js
@@ -22,9 +22,14 @@ function main( options ) {
     picturesDir = options.picturesDir || '../pictures/';
 
   if ( workDir ) {
-    workDir = pathResolve( workDir );
-  } else {
-    workDir = process.cwd()
+    try {
+      process.chdir( workDir );
+    }
+    catch ( e ) {
+      console.error( '切换到工作目录时出错：' , e );
+      console.error( '请检查路径 %s 是否存在。' , pathResolve( workDir ) );
+      return;
+    }
   }
 
   if ( !picturesDir.endsWith( '/' ) ) {
@@ -35,14 +40,12 @@ function main( options ) {
   let templateCount = 0 , // 寻找到了多少个模板
     htmlCount = 0; // 一共生成了多少个 html 文件
 
-  log( '\n正在 %s 里寻找是否有匹配的模板……' , workDir );
+  log( '\n正在 %s 里寻找是否有匹配的模板……' , process.cwd() );
 
   templates()
     .then( files => {
       const globPattern = '{' + files.join( ',' ) + '}/' + xlsxFilename;
-      glob( globPattern , {
-        cwd : workDir
-      } , function ( err , dataXLSXFiles ) {
+      glob( globPattern , function ( err , dataXLSXFiles ) {
         if ( err ) {
           console.dir( '查找文件时出错：' , err );
           return;
@@ -64,16 +67,16 @@ function main( options ) {
     // 复制模板所需的静态文件
     fsp.copy(
       pathResolve( packageDir , './libs/templates/' + templateName + '/resources' ) ,
-      pathResolve( workDir , './' + templateName + '/resources' )
+      templateName + '/resources'
     );
 
     // 读取电子表格的第一张表
     let wb;
     try {
-      wb = parseXLSX( pathResolve( workDir , fileRelativePath ) );
+      wb = parseXLSX( fileRelativePath );
     }
     catch ( e ) {
-      console.error( '解析 %s 文件时出错：' , pathResolve( workDir , fileRelativePath ) );
+      console.error( '解析 %s 文件时出错：' , fileRelativePath );
       console.error( e );
       console.error( '请检查文件格式是否正确。' );
     }
@@ -82,7 +85,7 @@ function main( options ) {
     renderer( firstSheet , templateName , {
       locals : { picturesDir } ,
       onRendered : function ( rowData , html ) {
-        const destPath = pathResolve( workDir , templateName + '/' + rowData[ '分单号' ] + '.html' );
+        const destPath = templateName + '/' + rowData[ '分单号' ] + '.html';
         return fsp
           .writeFile( destPath , html )
           .then( ()=> {
