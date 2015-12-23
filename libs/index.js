acf71ced67bb1c9e17342fbce88f0babb6767073
Milk Lee <milk.lee@qq.com>
2015-12-23 11:09:46 +0800

    使用 node-glob 匹配文件

--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,9 @@
 'use strict';
-
-const fsp = require( 'fs-promise' ) ,
-  pathResolve = require( 'path' ).resolve ,
+const glob = require( 'glob' ) ,
+  fsp = require( 'fs-promise' ) ,
+  path = require( 'path' ) ,
+  pathResolve = path.resolve ,
+  pathDir = path.dirname ,
   parseXLSX = require( 'xlsx' ).readFile;
 
 const packageDir = pathResolve( __dirname , '../' ); // 此 npm 包在本地系统的路径，用于定位模板位置
@@ -31,69 +33,63 @@ function main( options ) {
   let templateCount = 0 , // 寻找到了多少个模板
     htmlCount = 0; // 一共生成了多少个 html 文件
 
-  Promise.all( [
-    templates() ,
-    fsp.readdir( workDir )
-  ] ).then( ( args )=> {
-    const templates = args[ 0 ] , files = args[ 1 ];
+  log( '\n正在 %s 里寻找是否有匹配的模板……' , workDir );
 
-    log( '\n正在 %s 里寻找是否有匹配的模板……' , workDir );
-
-    files.forEach( ( maybeTemplateName )=> {
-      if ( templates.indexOf( maybeTemplateName ) >= 0 ) { // 如果文件夹的名字是其中一个模板的名字
-        fsp
-          .stat( pathResolve( workDir , maybeTemplateName ) )
-          .then( ( stat )=> {
-            if ( stat.isDirectory() ) {
-              handle( maybeTemplateName );
-            }
-          } );
-      }
-    } );
-
-    function handle( templateName ) {
-      log( '找到模板：' , templateName );
-      templateCount += 1;
-
-      // 复制模板所需的静态文件
-      fsp.copy(
-        pathResolve( packageDir , './libs/templates/' + templateName + '/resources' ) ,
-        pathResolve( workDir , './' + templateName + '/resources' )
-      );
-
-      // 读取电子表格的第一张表
-      let wb;
-      try {
-        wb = parseXLSX( pathResolve( workDir , templateName , xlsxFilename ) );
-      }
-      catch ( e ) {
-        console.error( '解析 %s 文件时出错：' , pathResolve( workDir , templateName , xlsxFilename ) );
-        console.error( e );
-        console.error( '将不会在 %s 里生成文件。' , templateName );
-        return Promise.resolve();
-      }
-      const firstSheet = wb.Sheets[ wb.SheetNames[ 0 ] ];
-
-      const renderFunc = require( './templates/' + templateName + '/index.js' );
-      renderFunc( firstSheet , function onRendered( rowData , html ) {
-        const destPath = pathResolve( workDir , templateName + '/' + rowData[ '分单号' ] + '.html' );
-        return fsp
-          .writeFile( destPath , html )
-          .then( ()=> {
-            htmlCount += 1;
-            log( '生成文件：' , destPath );
-          } );
-      } , {
-        picturesDir
+  templates()
+    .then( files => {
+      const globPattern = '{' + files.join( ',' ) + '}/' + xlsxFilename;
+      glob( globPattern , {
+        cwd : workDir
+      } , function ( err , dataXLSXFiles ) {
+        if ( err ) {
+          console.dir( '查找文件时出错：' , err );
+          return;
+        }
+        dataXLSXFiles.forEach( handle );
       } );
-    }
-  } , ( err )=> {
-    log( '读取时发生错误：' + err );
-  } );
+    } );
 
   process.on( 'exit' , function () {
     log( '\n运行完毕，此次运行发现了 %s 个模板文件夹，共生成 %s 个 html 文件。' , templateCount , htmlCount );
   } );
+
+  function handle( fileRelativePath ) {
+    const templateName = pathDir( fileRelativePath );
+
+    log( '找到模板：' , templateName );
+    templateCount += 1;
+
+    // 复制模板所需的静态文件
+    fsp.copy(
+      pathResolve( packageDir , './libs/templates/' + templateName + '/resources' ) ,
+      pathResolve( workDir , './' + templateName + '/resources' )
+    );
+
+    // 读取电子表格的第一张表
+    let wb;
+    try {
+      wb = parseXLSX( pathResolve( workDir , fileRelativePath ) );
+    }
+    catch ( e ) {
+      console.error( '解析 %s 文件时出错：' , pathResolve( workDir , fileRelativePath ) );
+      console.error( e );
+      console.error( '请检查文件格式是否正确。' );
+    }
+    const firstSheet = wb.Sheets[ wb.SheetNames[ 0 ] ];
+
+    const renderFunc = require( './templates/' + templateName + '/index.js' );
+    renderFunc( firstSheet , function onRendered( rowData , html ) {
+      const destPath = pathResolve( workDir , templateName + '/' + rowData[ '分单号' ] + '.html' );
+      return fsp
+        .writeFile( destPath , html )
+        .then( ()=> {
+          htmlCount += 1;
+          log( '生成文件：' , destPath );
+        } );
+    } , {
+      picturesDir
+    } );
+  }
 
   function log() {
     console.log.apply( console , arguments );
