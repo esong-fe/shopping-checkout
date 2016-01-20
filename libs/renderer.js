658a5cb0a3649e546a29c7174e21a3f28d2ba20e
Milk Lee <milk.lee@qq.com>
2016-01-20 11:56:42 +0800

    添加一点注释

--- libs/renderer.js
+++ libs/renderer.js
@@ -6,9 +6,10 @@ const parseXLSX = require( 'xlsx' ).readFile;
 /**
  * @param {String} sheetPath - Excel 表格的绝对路径
  * @param {String} templateName - 模板名称
- * @param {Object} [options]
+ * @param {Object} options
+ * @param {Boolean} [options.useCN] - 是否使用谷歌中文翻译
  * @param {Object} [options.locals] - 要附加在 rowData 上的通用数据
- * @param {Function} [options.onRendered(rowData:*,html:String)] - 没当一个模板生成好了之后就会调用此函数
+ * @param {Function} options.onParsed(rowData:Object) - 每当一个模板生成好了之后就会调用此函数
  */
 module.exports = ( sheetPath , templateName , options )=> {
   // 读取电子表格的第一张表
